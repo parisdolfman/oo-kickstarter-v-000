d816959ede5e565b4f8e3e8704a9912696bbabb8
Paris Dolfman <parisdolfman@gmail.com>
2019-08-14 19:27:26 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -4,11 +4,12 @@ attr_reader :title, :backers
 
 def initialize(title)
   @title = title
-  @backers = []
+  @@backers = []
 end
 
 def add_backer(backer)
-  @backers << backer
+  @@backers << backer
+  backer.back_project(self) unless backer.backed_projects.include?(self)
 end
 
 
